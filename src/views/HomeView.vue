24191449273b1601892427e53d29db373ab3b293
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2024-04-20 14:54:43 +0530

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div v-on:click="count++">
+    Hello from home
+
+    {{ count }}
+  </div>
+</template>
